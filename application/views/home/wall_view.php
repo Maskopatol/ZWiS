60e00771e18a6717507b6fe8895ffbb4d068b28d
Mateusz Russak <raay.max@gmail.com>
2012-11-28 23:38:28 +0100

    Takie pierdoły ze znakami małymi i dużymi...

--- application/views/home/wall_view.php
+++ application/views/home/wall_view.php
@@ -2,7 +2,7 @@
 <body>
 <h1><?=$heading?></h1>
 
-<?=form_open('User/add_post');?>
+<?=form_open('user/add_post');?>
 
 <textarea rows="5" cols="20" name="post_content">
 Napisz posta
@@ -13,16 +13,16 @@ Napisz posta
 	<?php foreach($posts as $pst): ?>
 	
 
-			<div><h3><?=anchor('User/info/'.$pst->id_user, $pst->name); ?></h3>
+			<div><h3><?=anchor('user/info/'.$pst->id_user, $pst->name); ?></h3>
 			<?php echo $pst->post_date; ?>
 			<p><?php echo $pst->post_content; ?></p>
 			<ul>
 				<?php foreach($pst->comments as $comment): ?>
-					<li><p><?=anchor('User/info/'.$comment->id_user, $comment->name); ?></p>
+					<li><p><?=anchor('user/info/'.$comment->id_user, $comment->name); ?></p>
 					<p><?php echo $comment->comment_content; ?></p>
 					<p><?php echo $comment->comment_date; ?></p></li>
 				<?php endforeach; ?>
-			<?=form_open('User/add_comment');?>
+			<?=form_open('user/add_comment');?>
 
 			<textarea rows="2" cols="20" name="comment_content">Komentarz</textarea>
 			<input type="submit" value="Dodaj komentarz" >
