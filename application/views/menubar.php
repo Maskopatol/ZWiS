4fce83c6cb8fca499344703ab1ed2d070b442af8
Mateusz Russak <raay.max@gmail.com>
2012-11-29 00:31:00 +0100

    Poprawki w stylach.. Upiększanie itp.

--- application/views/menubar.php
+++ application/views/menubar.php
@@ -1,3 +1,6 @@
-<div class="menu-top">
-	<?=anchor("user/","Ściana");?> | Wiadomości | Ustawienia | Znajomi | Uczelnie | <?=anchor("logout/","Wyloguj");?>
-</div>
+<div class="item"><?=anchor("user/","Ściana");?></div>
+<div class="item">Znajomi </div>
+<div class="item">Uczelnie </div>
+<div class="item">Wiadomości </div>
+<div class="item">Ustawienia </div>
+<div class="item"><?=anchor("logout/","Wyloguj");?></div>
